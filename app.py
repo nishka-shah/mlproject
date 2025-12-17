ffb4fa2415b90ab1d82b3fcf9cd9192ca477f1c8
nishka-shah <155789586+nishka-shah@users.noreply.github.com>
2025-12-16 23:25:52 -0500

    Update app.py 2

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
 
 if __name__ == "__main__":
-    # changed port to 8000
+    # changed the port to 8000
     app.run(host='0.0.0.0', port=8000)
